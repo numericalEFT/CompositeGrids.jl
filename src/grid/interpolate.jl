581ffd1e0a8db2dab2ff512302a5b8a22656fbe9
Xiansheng Cai <iintsjds@gmail.com>
2021-12-29 17:15:56 -0500

    add 1d and 2d optimization for linearND, now redirect to linear1D and linear2D

--- src/grid/interpolate.jl
+++ src/grid/interpolate.jl
@@ -271,6 +271,81 @@ function linearND(data, xgrids, xs)
     return f(as, xs)
 end
 
+@inline function linearND(data::Matrix, xgrids, xs)
+    # for 2d, use linear2D
+    @assert length(xgrids)==length(xs)==2
+    return linear2D(data, xgrids[1],xgrids[2],xs[1],xs[2])
+end
+
+@inline function linearND(data::Vector, xgrids, xs)
+    # for 2d, use linear2D
+    @assert length(xgrids)==length(xs)==1
+    return linear2D(data, xgrids[1],xs[1])
+end
+
+"""
+   linear2D(data, xgrid, ygrid, x, y) 
+
+linear interpolation of data(x, y)
+
+#Arguments:
+- xgrid: one-dimensional grid of x
+- ygrid: one-dimensional grid of y
+- data: two-dimensional array of data
+- x: x
+- y: y
+"""
+@inline function linear2D(data, xgrid, ygrid, x, y)
+
+    xarray, yarray = xgrid.grid, ygrid.grid
+
+    # if (
+    #     x <= xarray[firstindex(xgrid)] ||
+    #     x >= xarray[lastindex(xgrid)] ||
+    #     y <= yarray[firstindex(ygrid)] ||
+    #     y >= yarray[lastindex(ygrid)]
+    # )
+    #     return 0.0
+    # end
+    # xi0, yi0 = floor(xgrid, x), floor(ygrid, y)
+    # xi1, yi1 = xi0 + 1, yi0 + 1
+
+    xi0,xi1,yi0,yi1 = 0,0,0,0
+    if(x<=xarray[firstindex(xgrid)])
+        xi0=1
+        xi1=2
+    elseif(x>=xarray[lastindex(xgrid)])
+        xi0=lastindex(xgrid)-1
+        xi1=xi0+1
+    else
+        xi0=floor(xgrid,x)
+        xi1=xi0+1
+    end
+
+    if(y<=yarray[firstindex(ygrid)])
+        yi0=1
+        yi1=2
+    elseif(y>=yarray[lastindex(ygrid)])
+        yi0=lastindex(ygrid)-1
+        yi1=yi0+1
+    else
+        yi0=floor(ygrid,y)
+        yi1=yi0+1
+    end
+
+    dx0, dx1 = x - xarray[xi0], xarray[xi1] - x
+    dy0, dy1 = y - yarray[yi0], yarray[yi1] - y
+
+    d00, d01 = data[xi0, yi0], data[xi0, yi1]
+    d10, d11 = data[xi1, yi0], data[xi1, yi1]
+
+    g0 = data[xi0, yi0] * dx1 + data[xi1, yi0] * dx0
+    g1 = data[xi0, yi1] * dx1 + data[xi1, yi1] * dx0
+
+    gx = (g0 * dy1 + g1 * dy0) / (dx0 + dx1) / (dy0 + dy1)
+    return gx
+end
+
 """
     function linear1D(data, xgrid, x)
 
