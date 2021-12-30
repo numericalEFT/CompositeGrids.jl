c386974a966718f35d6e79612ef82ff755f62197
Xiansheng Cai <iintsjds@gmail.com>
2021-12-29 19:37:14 -0500

    add compat for JLD2 and FileIO; corrected deepequal for io test

--- test/io.jl
+++ test/io.jl
@@ -1,23 +1,55 @@
 @testset "IO" begin
 	  @testset "JLD2" begin
+        function deepequal(a::Array, b::Array)
+            typeof(a) == typeof(b) || return false
+            N = length(a)
+            for i in 1:N
+                deepequal(a[i] , b[i]) || return false
+            end
+            return true
+        end
+
         function deepequal(a, b)
             typeof(a) == typeof(b) || return false
             N = fieldcount(typeof(a))
+            if N==0
+                return a==b
+            end
             for i in 1:N
                 deepequal(getfield(a, i) , getfield(b, i)) || return false
             end
             return true
         end
 
+        function deeptest(a::Array, b::Array)
+            @test typeof(a) == typeof(b) 
+            N = length(a)
+            for i in 1:N
+                deeptest(a[i] , b[i])
+            end
+        end
+
+        function deeptest(a, b)
+            @test typeof(a) == typeof(b) 
+            N = fieldcount(typeof(a))
+            if N==0
+                @test a==b
+            end
+            for i in 1:N
+                deeptest(getfield(a, i) , getfield(b, i))
+                @test deepequal(a,b)
+            end
+        end
+
 	      β = 10
 
         tgrid = CompositeGrid.LogDensedGrid(
-        :gauss,# The top layer grid is :gauss, optimized for integration. For interpolation use :cheb
-        [0.0, β],# The grid is defined on [0.0, β]
-        [0.0, β],# and is densed at 0.0 and β, as given by 2nd and 3rd parameter.
-        5,# N of log grid
-        0.005, # niminum interval length of log grid
-        5 # N of bottom layer
+            :gauss,# The top layer grid is :gauss, optimized for integration. For interpolation use :cheb
+            [0.0, β],# The grid is defined on [0.0, β]
+            [0.0, β],# and is densed at 0.0 and β, as given by 2nd and 3rd parameter.
+            5,# N of log grid
+            0.005, # niminum interval length of log grid
+            5 # N of bottom layer
         )
 
         ############# FileIO API #################
@@ -27,7 +59,8 @@
         dg = d["grid"]
         println(typeof(dg))
         println(dg.grid)
-        @test deepequal(dg, tgrid)
+        deeptest(dg, tgrid)
+        @test deepequal(dg,tgrid)
 
         ############# naive API ##################
         jldopen("example.jld2", "w") do file
@@ -38,7 +71,8 @@
             g = file["test"]
             println(typeof(g))
             println(g.grid)
-            @test deepequal(g, tgrid)
+            deeptest(g, tgrid)
+            @test deepequal(g,tgrid)
         end
 
         # remove the file
