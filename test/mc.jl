09dc2a3d530d07126c8069f4aefadf647a3118b2
Xiansheng Cai <iintsjds@gmail.com>
2022-09-15 13:56:51 -0400

    added more tests for locate and volumn

--- test/mc.jl
+++ test/mc.jl
@@ -2,50 +2,80 @@
     # testing locate and volumn functions provided for monte carlo
     rng = MersenneTwister(1234)
 
-    @testset "Locate and Volumn" begin
-        Ng = 33
-        β = π
-        tgrid = SimpleGrid.Uniform{Float64}([0.0, β], Ng)
-        δ = 1e-6
-
+    function test_locate_volumn(grid; δ = 1e-6)
         vol = 0.0
-        for (ti, t) in enumerate(tgrid.grid)
+        for (ti, t) in enumerate(grid.grid)
             # test locate
-            @test ti == Interp.locate(tgrid, t)
-            @test ti == Interp.locate(tgrid, (ti == 1) ? t : t - δ)
-            @test ti == Interp.locate(tgrid, (ti == length(tgrid)) ? t : t + δ)
-            vol += Interp.volumn(tgrid, ti)
+            @test ti == Interp.locate(grid, t)
+            @test ti == Interp.locate(grid, (ti == 1) ? t : t - δ)
+            @test ti == Interp.locate(grid, (ti == length(grid)) ? t : t + δ)
+            vol += Interp.volumn(grid, ti)
         end
 
-        @test vol == Interp.volumn(tgrid)
+        @test vol ≈ Interp.volumn(grid)
+    end
+
+    @testset "Locate and Volumn" begin
+        Ng, Ns = 10, 4
+        β = π
+
+        tgrid = SimpleGrid.Uniform{Float64}([0.0, β], Ng)
+        test_locate_volumn(tgrid)
+
+        tgrid = SimpleGrid.BaryCheb{Float64}([0.0, β], Ng)
+        test_locate_volumn(tgrid)
+
+        tgrid = SimpleGrid.Log{Float64}([0.0, β], Ng, 0.001, true)
+        test_locate_volumn(tgrid)
+
+        tgrid = CompositeGrid.LogDensedGrid(:cheb, [0.0, β],[β/2,], Ng, 0.001, Ns)
+        test_locate_volumn(tgrid)
 
     end
 
-    @testset "MC histogram" begin
-        Nmc = 1e6
-        Ng = 33
-        β = π
-        tgrid = SimpleGrid.Uniform{Float64}([0.0, β], Ng)
-        # tugrid = Grid.Uniform{Float64,33}(0.0, β, (true, true))
-        # kugrid = Grid.Uniform{Float64,33}(0.0, maxK, (true, true))
-        f(t) = t
+    function test_mc_histogram(grid; Npp=1e6, f = (x -> x))
+        # println("testing:$(typeof(grid))")
+        Ng = length(grid)
+        a, b = grid.bound[1], grid.bound[2]
+        Nmc = Npp * Ng
 
-        hist = zeros(tgrid.size)
+        hist = zeros(Ng)
         for i in 1:Nmc
-            x = rand(rng) * β
-            ind = Interp.locate(tgrid, x)
-            vol = Interp.volumn(tgrid, ind)
-            hist[ind] += f(x) / vol * Interp.volumn(tgrid)
+            x = rand(rng) * (b - a) + a
+            ind = Interp.locate(grid, x)
+            vol = Interp.volumn(grid, ind)
+            hist[ind] += f(x) / vol * Interp.volumn(grid)
         end
 
-        for (ti, t) in enumerate(tgrid.grid)
+        for (ti, t) in enumerate(grid.grid)
+            vi = Interp.volumn(grid, ti)
+            vall = Interp.volumn(grid)
             if ti != 1 && ti != Ng
-                @test isapprox(hist[ti] / Nmc, f(t), rtol=3 / sqrt(Nmc / Ng))
+                @test isapprox(hist[ti] / Nmc, f(t),
+                               rtol=5 / sqrt(Nmc*vi/vall), atol = 5*vi/vall)
             else
                 # edge points has extra error because grid point is not at center of interval
-                @test isapprox(hist[ti] / Nmc, f(t), rtol=3 / sqrt(Nmc / Ng), atol = 3/Ng)
+                @test isapprox(hist[ti] / Nmc, f(t),
+                               rtol=5 / sqrt(Nmc*vi/vall), atol = 5*vi/vall)
             end
         end
+    end
+
+    @testset "MC histogram" begin
+        Ng, Nl, Ns = 16, 8, 4
+        β = π
+
+        tgrid = SimpleGrid.Uniform{Float64}([0.0, β], Ng)
+        test_mc_histogram(tgrid)
+
+        tgrid = SimpleGrid.BaryCheb{Float64}([0.0, β], Nl)
+        test_mc_histogram(tgrid)
+
+        tgrid = SimpleGrid.Log{Float64}([0.0, β], Nl, 0.1, true)
+        test_mc_histogram(tgrid)
+
+        tgrid = CompositeGrid.LogDensedGrid(:cheb, [0.0, β],[β/2,], Nl, 0.1, Ns)
+        test_mc_histogram(tgrid)
 
     end
 
